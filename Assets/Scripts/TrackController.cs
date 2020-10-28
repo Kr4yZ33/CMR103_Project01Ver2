7fa521fd555ba4bc2cae9680deb4ccce95f83ff4
jpernesz <1020894@student.sae.edu.au>
2020-10-28 15:12:27 +1100

    Created script for track piece alignment

---
+++ Assets/Scripts/TrackController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackController : MonoBehaviour
+{
+    public GameObject piece1;
+    public GameObject piece2;
+    public bool trackUpsideDown;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckTrackAlignment();
+    }
+
+    void CheckTrackAlignment()
+    {
+        if (Vector3.Dot(piece1.transform.up, piece2.transform.up) > 0.7f)
+        {
+            Debug.Log("Facing same direction UP");
+            // They are roughly facing the same upwards
+            trackUpsideDown = false;
+        }
+    }
+    
+}
