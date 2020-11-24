6cb383b1133aec39ce4a49a59394dcfa3000bf18
jpernesz <1020894@student.sae.edu.au>
2020-11-25 05:33:06 +1100

    Adjustments to track scripts

---
+++ Assets/Scripts/PlayerRespawn.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRespawn : MonoBehaviour
+{
+    public Transform playerRespawnTransform;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Respawn"))
+        {
+            gameObject.transform.position = playerRespawnTransform.transform.position;
+            gameObject.transform.rotation = playerRespawnTransform.transform.rotation;
+        }
+    }
+}
