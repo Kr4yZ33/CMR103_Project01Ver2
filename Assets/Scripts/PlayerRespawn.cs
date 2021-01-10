c2c5f06757602df6ec438cf937a16c20fc0319a5
jpernesz <1020894@student.sae.edu.au>
2021-01-11 00:07:48 +1100

    Commenting scripts

--- Assets/Scripts/PlayerRespawn.cs
+++ Assets/Scripts/PlayerRespawn.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class PlayerRespawn : MonoBehaviour
 {
-    public Transform playerRespawnTransform;
+    public Transform playerRespawnTransform; // reference to the respawn transform
 
+    /// <summary>
+    /// on trigger enter
+    /// </summary>
+    /// <param name="other"></param>
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Respawn"))
+        if (other.CompareTag("Respawn")) // if this thing colliding with us is tagged Respawn
         {
-            gameObject.transform.position = playerRespawnTransform.transform.position;
-            gameObject.transform.rotation = playerRespawnTransform.transform.rotation;
+            gameObject.transform.position = playerRespawnTransform.transform.position; // set the transform position of this game object to the respwan transform
+            gameObject.transform.rotation = playerRespawnTransform.transform.rotation; // set the transform rotation of this game object to the respwan rotation
         }
     }
 }
